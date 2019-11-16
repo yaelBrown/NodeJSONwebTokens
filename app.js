036811a75703bf3527cbe33d770b32127f72e5bd
Yael Brown <yaelrbrown@gmail.com>
2019-11-15 23:46:30 -0600

    added routes and started verify token method.

--- app.js
+++ app.js
@@ -5,4 +5,44 @@ const jwt = require('jsonwebtoken');
 
 app.get('/', (req, res) => res.json({message: 'Hello World!'}));
 
+app.get('/api/posts', (req, res) => {
+  res.json({
+    message: 'Viewing all the posts'
+  });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+  res.json({
+    message: 'Post created !'
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  // Mock user, to be authenticated
+  const user= {
+    id: 1,
+    username: "test",
+    password: "testpw"
+  }
+
+  // sends information about user asynchonously.
+  jwt.sign({user}, 'secretKey', (err, token) => {
+    res.json({
+      token
+    })
+  });
+});
+
+// Format of token
+// Athorization: Bearer <access_token>
+
+// Verify Token
+// (Have to use es5 syntax for some reason)
+function verifyToken (req, res, next) {
+   // Get auth header value
+   const bearerHeader = req.headers['authorization'];
+
+   // Check if bearer is undefined
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
